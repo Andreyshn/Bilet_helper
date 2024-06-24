b63532fcb43b12060e673ddf9bba37a2d6bf8789
Andrew <62589086+Andreyshn@users.noreply.github.com>
2024-06-24 05:24:59 +0300

    Final

--- insert.py
+++ insert.py
@@ -1,14 +1,6 @@
-from hmac import new
-from xml.dom.minidom import Element
 from docx import Document
-'''
-ex() выполняет роль поиска таблицы из шаблона
-и записывает ее в header_table
-После этого fill() выделяет секцию документа, из секции заголовок
-И вставляет туда таблицу напрямую
-После этого записывает вопросы из готового списка(сейчас реализовано 3 вопроса)
-Далее идет разрыв страницы
-'''
+import copy
+
 def ex(doc):
     for elem in doc.element.body:
         if elem.tag.endswith('tbl'):
@@ -17,20 +9,21 @@ def ex(doc):
         print('Неверный формат шаблонного документа!')
         return 1
     
-def fill(rdy, theme_count, header_table, newDoc):
-    section = newDoc.sections[0]
-    header = section.header     
-    header._element.append(header_table)
-    for i in range(2, len(rdy), 3):
+def fill(rdy, theme_count, table, newDoc):
+    g = copy.deepcopy(table)
+    for i in range(len(rdy)):
+        newDoc._body._element._insert_tbl(table)
+        del table
+        table = copy.deepcopy(g)
         newDoc.add_paragraph('\n')
-        for j in range(i-2,i+1):
-            newDoc.add_paragraph(f'{(j)%3+1}. {rdy[j]}')
-        newDoc.add_page_break()
+        newDoc.add_heading(f'Билет № {i+1}', level = 2)
+        for j in range(theme_count):
+            newDoc.add_paragraph(f'{(j)%theme_count+1}. {rdy[i][j]}')
+        newDoc.add_paragraph('\n')
         newDoc.save('ready.docx')
 
 def ins(rdy, theme_count):
     temp = Document('template.docx')
     newDoc = Document()
-    header_table = None
-    header_table = ex(temp)
-    fill(rdy, theme_count, header_table, newDoc)
+    table = ex(temp)
+    fill(rdy, theme_count, table, newDoc)
